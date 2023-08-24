3c87d03314a6867b62b9faefb8f075fbde346762
Marcio Souza <marciooliveira10@gmail.com>
2023-08-24 08:58:39 +0100

    created classes

--- src/main/java/com/repconnect/rc/models/InvoiceData.java
+++ src/main/java/com/repconnect/rc/models/InvoiceData.java
@@ -2,27 +2,19 @@ package com.repconnect.rc.models;
 
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.sql.Timestamp;
 import java.util.Date;
 import java.util.UUID;
-
 @Entity
-@Table(name= "TB_SALES")
+@Table(name="TB_INVOICE_DATA")
 @Getter
-@Setter
-public class SalesModel implements Serializable {
+public class InvoiceData implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID uuid;
-    private BigDecimal value;
-    private Date saleDate;
-    private Timestamp createdAt;
-    private Timestamp updatedAt;
-    private boolean proforma;
-
+    private String pdfLink;
+    private String excelLink;
+    private Date date;
 }
